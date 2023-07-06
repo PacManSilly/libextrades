71ec409c2fd0c8875375f5eb8df07c12becf58da
realestkma <kelvinmayoayeni@gmail.com>
2023-07-06 22:44:15 +0100

    changed axios baseURL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,9 +7,7 @@ import axios from "axios";
 import "./assets/index.css";
 
 // axios settings
-axios.defaults.baseURL = "http://192.168.1.104:8080/";
-// axios.defaults.baseURL = "http://localhost:8000/";
-// axios.defaults.baseURL = "https://libextrades.com/";
+axios.defaults.baseURL = "https://libextrades.com/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 axios.defaults.timeout = 9000;
